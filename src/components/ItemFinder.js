1c8f282042f1f30939bbdf77b1c98a65b426033a
Brandon Stone <brandonrstone@gmail.com>
2022-06-12 12:55:12 -0400

    Item data updates

--- src/components/ItemFinder.js
+++ src/components/ItemFinder.js
@@ -3,101 +3,21 @@ import React, { useState, useMemo } from 'react'
 export default function ItemFinder({ UniqueItems, SetItems, Runewords }) {
   const [search, setSearch] = useState('')
 
-  const Runeword = props => (
-    <div className='runeword-container'>
-      {props.T1Code1 && props.T1Code2 && (
-        <div>
-          <div className='runeword-name'>{props.Name}</div>
-          <div className='item-base'>{props.itype1}</div>
-          <div className='runeword-runes'>
-            '{props.Rune1}
-            {props.Rune2}
-            {props.Rune3}
-            {props.Rune4}
-            {props.Rune5}'
-          </div>
-          {props.T1Min1 !== props.T1Max1 && props.T1Min1 < props.T1Max1 && (
-            <div>
-              {props.T1Code1 !== 'oskill' &&
-                props.T1Code1 !== 'hit-skill' &&
-                props.T1Code1 !== 'gethit-skill' &&
-                props.T1Code1 !== 'charged' && <span>{props.T1Code1}</span>}
-              <div>{props.T1Param1 && <span>{props.T1Param1}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min1}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max1}</span>
-            </div>
-          )}
-          {props.T1Min2 !== props.T1Max2 && props.T1Min2 < props.T1Max2 && (
-            <div>
-              {props.T1Code2 !== 'oskill' &&
-                props.T1Code2 !== 'hit-skill' &&
-                props.T1Code2 !== 'gethit-skill' &&
-                props.T1Code2 !== 'charged' && <span>{props.T1Code2}</span>}
-              <div>{props.T1Param2 && <span>{props.T1Param2}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min2}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max2}</span>
-            </div>
-          )}
-          {props.T1Min3 !== props.T1Max3 && props.T1Min3 < props.T1Max3 && (
-            <div>
-              {props.T1Code3 !== 'oskill' &&
-                props.T1Code3 !== 'hit-skill' &&
-                props.T1Code3 !== 'gethit-skill' &&
-                props.T1Code3 !== 'charged' && <span>{props.T1Code3}</span>}
-              <div>{props.T1Param3 && <span>{props.T1Param3}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min3}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max3}</span>
-            </div>
-          )}
-          {props.T1Min4 !== props.T1Max4 && props.T1Min4 < props.T1Max4 && (
-            <div>
-              {props.T1Code4 !== 'oskill' &&
-                props.T1Code4 !== 'gethit-skill' &&
-                props.T1Code4 !== 'charged' && <span>{props.T1Code4}</span>}
-              <div>{props.T1Param4 && <span>{props.T1Param4}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min4}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max4}</span>
-            </div>
-          )}
-          {props.T1Min5 !== props.T1Max5 && props.T1Min5 < props.T1Max5 && (
-            <div>
-              {props.T1Code5 !== 'oskill' &&
-                props.T1Code5 !== 'gethit-skill' &&
-                props.T1Code5 !== 'charged' && <span>{props.T1Code5}</span>}
-              <div>{props.T1Param5 && <span>{props.T1Param5}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min5}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max5}</span>
-            </div>
-          )}
-          {props.T1Min6 !== props.T1Max6 && props.T1Min6 < props.T1Max6 && (
-            <div>
-              {props.T1Code6 !== 'oskill' &&
-                props.T1Code6 !== 'aura' &&
-                props.T1Code6 !== 'charged' && <span>{props.T1Code6}</span>}
-              <div>{props.T1Param6 && <span>{props.T1Param6}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min6}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max6}</span>
-            </div>
-          )}
-          {props.T1Min7 !== props.T1Max7 && props.T1Min7 < props.T1Max7 && (
-            <div>
-              {props.T1Code7 !== 'oskill' &&
-                props.T1Code7 !== 'aura' &&
-                props.T1Code7 !== 'charged' && <span>{props.T1Code7}</span>}
-              <div>{props.T1Param6 && <span>{props.T1Param7}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min7}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max7}</span>
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  )
-
   const UniqueItem = props => (
     <div className='unique-item-container'>
       <div className='unique-item-name'>{props.name}</div>
-      <div className='item-base'>{props.itemBase}</div>
+      <div
+        className='item-base'
+        style={
+          props.itemBase === 'Small Charm' ||
+          props.itemBase === 'Large Charm' ||
+          props.itemBase === 'Grand Charm'
+            ? { color: 'rgb(201, 175, 111)' }
+            : { color: 'rgb(110, 108, 108)' }
+        }
+      >
+        {props.itemBase}
+      </div>
       {props.min1 !== props.max1 && props.min1 < props.max1 && (
         <div>
           {props.prop1 !== 'oskill' &&
@@ -301,6 +221,97 @@ export default function ItemFinder({ UniqueItems, SetItems, Runewords }) {
           <div></div>
           <span className='set-item-min-roll'>{props.min9}</span> -{' '}
           <span className='set-item-max-roll'>{props.max9}</span>
+        </div>
+      )}
+    </div>
+  )
+
+  const Runeword = props => (
+    <div className='runeword-container'>
+      {props.T1Code1 && props.T1Code2 && (
+        <div>
+          <div className='runeword-name'>{props.Name}</div>
+          <div className='item-base'>{props.itype1}</div>
+          <div className='runeword-runes'>
+            '{props.Rune1}
+            {props.Rune2}
+            {props.Rune3}
+            {props.Rune4}
+            {props.Rune5}'
+          </div>
+          {props.T1Min1 !== props.T1Max1 && props.T1Min1 < props.T1Max1 && (
+            <div>
+              {props.T1Code1 !== 'oskill' &&
+                props.T1Code1 !== 'hit-skill' &&
+                props.T1Code1 !== 'gethit-skill' &&
+                props.T1Code1 !== 'charged' && <span>{props.T1Code1}</span>}
+              <div>{props.T1Param1 && <span>{props.T1Param1}</span>}</div>
+              <span className='runeword-min-roll'>{props.T1Min1}</span> -{' '}
+              <span className='runeword-max-roll'>{props.T1Max1}</span>
+            </div>
+          )}
+          {props.T1Min2 !== props.T1Max2 && props.T1Min2 < props.T1Max2 && (
+            <div>
+              {props.T1Code2 !== 'oskill' &&
+                props.T1Code2 !== 'hit-skill' &&
+                props.T1Code2 !== 'gethit-skill' &&
+                props.T1Code2 !== 'charged' && <span>{props.T1Code2}</span>}
+              <div>{props.T1Param2 && <span>{props.T1Param2}</span>}</div>
+              <span className='runeword-min-roll'>{props.T1Min2}</span> -{' '}
+              <span className='runeword-max-roll'>{props.T1Max2}</span>
+            </div>
+          )}
+          {props.T1Min3 !== props.T1Max3 && props.T1Min3 < props.T1Max3 && (
+            <div>
+              {props.T1Code3 !== 'oskill' &&
+                props.T1Code3 !== 'hit-skill' &&
+                props.T1Code3 !== 'gethit-skill' &&
+                props.T1Code3 !== 'charged' && <span>{props.T1Code3}</span>}
+              <div>{props.T1Param3 && <span>{props.T1Param3}</span>}</div>
+              <span className='runeword-min-roll'>{props.T1Min3}</span> -{' '}
+              <span className='runeword-max-roll'>{props.T1Max3}</span>
+            </div>
+          )}
+          {props.T1Min4 !== props.T1Max4 && props.T1Min4 < props.T1Max4 && (
+            <div>
+              {props.T1Code4 !== 'oskill' &&
+                props.T1Code4 !== 'gethit-skill' &&
+                props.T1Code4 !== 'charged' && <span>{props.T1Code4}</span>}
+              <div>{props.T1Param4 && <span>{props.T1Param4}</span>}</div>
+              <span className='runeword-min-roll'>{props.T1Min4}</span> -{' '}
+              <span className='runeword-max-roll'>{props.T1Max4}</span>
+            </div>
+          )}
+          {props.T1Min5 !== props.T1Max5 && props.T1Min5 < props.T1Max5 && (
+            <div>
+              {props.T1Code5 !== 'oskill' &&
+                props.T1Code5 !== 'gethit-skill' &&
+                props.T1Code5 !== 'charged' && <span>{props.T1Code5}</span>}
+              <div>{props.T1Param5 && <span>{props.T1Param5}</span>}</div>
+              <span className='runeword-min-roll'>{props.T1Min5}</span> -{' '}
+              <span className='runeword-max-roll'>{props.T1Max5}</span>
+            </div>
+          )}
+          {props.T1Min6 !== props.T1Max6 && props.T1Min6 < props.T1Max6 && (
+            <div>
+              {props.T1Code6 !== 'oskill' &&
+                props.T1Code6 !== 'aura' &&
+                props.T1Code6 !== 'charged' && <span>{props.T1Code6}</span>}
+              <div>{props.T1Param6 && <span>{props.T1Param6}</span>}</div>
+              <span className='runeword-min-roll'>{props.T1Min6}</span> -{' '}
+              <span className='runeword-max-roll'>{props.T1Max6}</span>
+            </div>
+          )}
+          {props.T1Min7 !== props.T1Max7 && props.T1Min7 < props.T1Max7 && (
+            <div>
+              {props.T1Code7 !== 'oskill' &&
+                props.T1Code7 !== 'aura' &&
+                props.T1Code7 !== 'charged' && <span>{props.T1Code7}</span>}
+              <div>{props.T1Param6 && <span>{props.T1Param7}</span>}</div>
+              <span className='runeword-min-roll'>{props.T1Min7}</span> -{' '}
+              <span className='runeword-max-roll'>{props.T1Max7}</span>
+            </div>
+          )}
         </div>
       )}
     </div>
