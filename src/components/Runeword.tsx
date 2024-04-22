5938af6d82026548378f018de38274e1c6304f87
Brandon Stone <brandonrstone@gmail.com>
2024-04-22 16:38:31 -0400

    Hopefully these changes to TypeScript don't break the app

--- src/components/Runeword.tsx
+++ src/components/Runeword.tsx
@@ -1,94 +1,26 @@
-import useItemValidation from '../hooks/useItemValidation'
+import { RunewordType } from '../types'
 
-const Runeword = (props: any): JSX.Element => {
-  const { isValidRunewordStat } = useItemValidation()
-
-  return (
-    <div className='runeword-container'>
-      {props.T1Code1 && props.T1Code2 && (
-        <div>
-          <div className='runeword-name'>{props.name}</div>
-          <div className='item-base'>{props.itype1}</div>
-          <div className='runeword-runes'>
-            '{props.Rune1}{props.Rune2}{props.Rune3}{props.Rune4}{props.Rune5}'
-          </div>
-          <div className='item-requirements'>
-            Required Level: {props.requiredLevel}
-          </div>
-          {props.T1Min1 !== props.T1Max1 && props.T1Min1 < props.T1Max1 && (
-            <div>
-              {isValidRunewordStat(props.T1Code1) && (
-                <span>{props.T1Code1}</span>
-              )}
-              <div>{props.T1Param1 && <span>{props.T1Param1}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min1}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max1}</span>
-            </div>
-          )}
-          {props.T1Min2 !== props.T1Max2 && props.T1Min2 < props.T1Max2 && (
-            <div>
-              {isValidRunewordStat(props.T1Code2) && (
-                <span>{props.T1Code2}</span>
-              )}
-              <div>{props.T1Param2 && <span>{props.T1Param2}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min2}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max2}</span>
-            </div>
-          )}
-          {props.T1Min3 !== props.T1Max3 && props.T1Min3 < props.T1Max3 && (
-            <div>
-              {isValidRunewordStat(props.T1Code3) && (
-                <span>{props.T1Code3}</span>
-              )}
-              <div>{props.T1Param3 && <span>{props.T1Param3}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min3}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max3}</span>
-            </div>
-          )}
-          {props.T1Min4 !== props.T1Max4 && props.T1Min4 < props.T1Max4 && (
-            <div>
-              {isValidRunewordStat(props.T1Code4) && (
-                <span>{props.T1Code4}</span>
-              )}
-              <div>{props.T1Param4 && <span>{props.T1Param4}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min4}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max4}</span>
-            </div>
-          )}
-          {props.T1Min5 !== props.T1Max5 && props.T1Min5 < props.T1Max5 && (
-            <div>
-              {isValidRunewordStat(props.T1Code5) && (
-                <span>{props.T1Code5}</span>
-              )}
-              <div>{props.T1Param5 && <span>{props.T1Param5}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min5}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max5}</span>
-            </div>
-          )}
-          {props.T1Min6 !== props.T1Max6 && props.T1Min6 < props.T1Max6 && (
-            <div>
-              {isValidRunewordStat(props.T1Code6) && (
-                <span>{props.T1Code6}</span>
-              )}
-              <div>{props.T1Param6 && <span>{props.T1Param6}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min6}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max6}</span>
-            </div>
-          )}
-          {props.T1Min7 !== props.T1Max7 && props.T1Min7 < props.T1Max7 && (
-            <div>
-              {isValidRunewordStat(props.T1Code7) && (
-                <span>{props.T1Code7}</span>
-              )}
-              <div>{props.T1Param6 && <span>{props.T1Param7}</span>}</div>
-              <span className='runeword-min-roll'>{props.T1Min7}</span> -{' '}
-              <span className='runeword-max-roll'>{props.T1Max7}</span>
-            </div>
-          )}
-        </div>
-      )}
+const Runeword = (runeward: RunewordType): JSX.Element => (
+  <div className='runeword-container'>
+    <div className='runeword-name'>{runeward.name}</div>
+    <div className='item-base'>{runeward.itype1}</div>
+    <div className='runeword-runes'>
+      '{runeward.Rune1}{runeward.Rune2}{runeward.Rune3}{runeward.Rune4}{runeward.Rune5}'
     </div>
-  )
-}
+    <div className='item-requirements'>Required Level: {runeward.requiredLevel}</div>
+    {[1, 2, 3, 4, 5, 6, 7].map((stat: number) => (
+      <>
+        {runeward[`Stat${stat}Min`] < runeward[`Stat${stat}Max`] && (
+          <div>
+            <div>{runeward[`Stat${stat}`] && <span>{runeward[`Stat${stat}`]}</span>}</div>
+            <span className='runeword-min-roll'>{runeward[`Stat${stat}Min`]}</span> -{' '}
+            <span className='runeword-max-roll'>{runeward[`Stat${stat}Max`]}</span>
+          </div>
+        )}
+      </>
+    ))}
+  </div>
+)
+
 
 export default Runeword
--- src/hooks/useItemValidation.tsx
+++ src/hooks/useItemValidation.tsx
@@ -1,18 +1,13 @@
 interface ItemValidationInterface {
   itemIsCharm: (props: any) => boolean
   isValidStat: (props: string) => boolean
-  isValidRunewordStat: (props: string) => boolean
 }
 
 const useItemValidation = (): ItemValidationInterface => {
-
   const itemIsCharm = (props: any) => !['Small Charm', 'Large Charm', 'Grand Charm'].includes(props?.name)
-
   const isValidStat = (props: string) => !['oskill', 'hit-skill', 'gethit-skill', 'charged'].includes(props)
 
-  const isValidRunewordStat = (props: string) => !['oskill', 'aura', 'charged'].includes(props)
-
-  return { itemIsCharm, isValidStat, isValidRunewordStat }
+  return { itemIsCharm, isValidStat }
 }
 
 export default useItemValidation
